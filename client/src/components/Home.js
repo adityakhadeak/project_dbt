49e63c79f49b94caa88a3f7156d571da1c5b8b29
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2024-07-28 23:03:06 +0530

    modified the algorithm for matching

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,26 +1,26 @@
 import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
 import { useFormik } from 'formik'
-import React, { useContext, useEffect, useState, CSSProperties } from 'react'
+import React, { useEffect, useState } from 'react'
 import * as Yup from 'yup'
 import * as XLSX from 'xlsx'
 import Fuse from 'fuse.js'
-import UserContext from '../context/UserContext.js'
 import { useNavigate } from 'react-router-dom'
 import BeatLoader from "react-spinners/BeatLoader";
 
 const Home = () => {
-    let [loading, setLoading] = useState(false);
-    let [color, setColor] = useState("#1b3058");
-    const navigate = useNavigate()
-    const [mergedData, setMergedData] = useState([])
-    const [file1Data, setFile1Data] = useState([])
-    const [file2Data, setFile2Data] = useState([])
+    const [loading, setLoading] = useState(false);
+    const [color, setColor] = useState("#1b3058");
+    const navigate = useNavigate();
+    const [mergedData, setMergedData] = useState([]);
+    const [file1Data, setFile1Data] = useState([]);
+    const [file2Data, setFile2Data] = useState([]);
 
     const css = {
         display: "block",
         margin: "0 auto",
         borderColor: "red",
     };
+
     const feeMapping = {
         'First Year': 124000,
         'Second Year': 137054,
@@ -28,24 +28,17 @@ const Home = () => {
         'Fourth Year': 130554
     };
 
-    // useEffect(() => {
-    //     if (!isLoggedIn) {
-    //         navigate('/login')
-    //     }
-    //     console.log(isLoggedIn)
-    // }, [user,isLoggedIn])
     useEffect(() => {
-        console.log(mergedData)
-    }, [mergedData])
-
+        console.log(mergedData);
+    }, [mergedData]);
 
     const standardizeName = (name) => {
         if (!name) return '';
 
         const parts = name.trim().split(/\s+/).sort();
         const newName = parts.join(' ');
-        console.log(newName)
-        return newName
+        console.log(newName);
+        return newName;
     };
 
     const formik = useFormik({
@@ -61,132 +54,156 @@ const Home = () => {
         }),
         onSubmit: async (values, actions) => {
             try {
-                setLoading(true)
+                setLoading(true);
 
-                const file1 = await readFileData(values.file1)
-                const file2 = await readFileData(values.file2)
+                const file1 = await readFileData(values.file1);
+                const file2 = await readMultipleSheets(values.file2);
 
-                setFile1Data(file1)
-                setFile2Data(file2)
+                setFile1Data(file1);
+                setFile2Data(file2);
 
-                await mergeData(file1, file2)
+                await mergeData(file1, file2);
 
-                setLoading(false)
+                setLoading(false);
 
-                actions.resetForm()
+                actions.resetForm();
             } catch (error) {
-                console.error("Error reading files:", error)
+                console.error("Error reading files:", error);
             }
         },
-    })
+    });
 
     const handleFileChange = (event) => {
-        const { name, files } = event.target
-        formik.setFieldValue(name, files[0])
+        const { name, files } = event.target;
+        formik.setFieldValue(name, files[0]);
     }
 
     const readFileData = (file) => {
         return new Promise((resolve, reject) => {
-            const reader = new FileReader()
+            const reader = new FileReader();
             reader.onload = (e) => {
-                const workbook = XLSX.read(e.target.result, { type: 'binary' })
-                const firstSheetName = workbook.SheetNames[0]
-                const worksheet = workbook.Sheets[firstSheetName]
-                const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 })
-                resolve(jsonData)
+                const workbook = XLSX.read(e.target.result, { type: 'binary' });
+                const firstSheetName = workbook.SheetNames[0];
+                const worksheet = workbook.Sheets[firstSheetName];
+                const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+                resolve(jsonData);
             }
-            reader.onerror = (error) => reject(error)
-            reader.readAsBinaryString(file)
-        })
+            reader.onerror = (error) => reject(error);
+            reader.readAsBinaryString(file);
+        });
+    }
+
+    const readMultipleSheets = (file) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const workbook = XLSX.read(e.target.result, { type: 'binary' });
+                let allData = [];
+                workbook.SheetNames.forEach(sheetName => {
+                    const worksheet = workbook.Sheets[sheetName];
+                    const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+                    allData = allData.concat(jsonData);
+                });
+                resolve(allData);
+            }
+            reader.onerror = (error) => reject(error);
+            reader.readAsBinaryString(file);
+        });
     }
 
     const mergeData = async (data1, data2) => {
-        const [header1, ...rows1] = data1
-        const [header2, ...rows2] = data2
+        const [header1, ...rows1] = data1;
+        const [header2, ...rows2] = data2;
 
         // Indices of relevant columns
-        const nameIndex1 = header1.indexOf('Name')
-        const admissionNoIndex1 = header1.indexOf('Admission No')
-        const yearNameIndex1 = header1.indexOf('YearName')
+        const nameIndex1 = header1.indexOf('Name');
+        const admissionNoIndex1 = header1.indexOf('Admission No');
+        const yearNameIndex1 = header1.indexOf('YearName');
 
-        const nameIndex2 = header2.indexOf('Beneficiary Name')
-        const schemeIndex2 = header2.indexOf('Scheme')
-        const applicationNoIndex2 = header2.indexOf('Application No')
-        const courseIndex2 = header2.indexOf('Course')
-        const disbursedAmountIndex2 = header2.indexOf('Disbursed Amount(₹)')
+        const nameIndex2 = header2.indexOf('Beneficiary Name');
+        const schemeIndex2 = header2.indexOf('Scheme');
+        const applicationNoIndex2 = header2.indexOf('Application No');
+        const courseIndex2 = header2.indexOf('Course');
+        const disbursedAmountIndex2 = header2.indexOf('Disbursed Amount(₹)');
 
         if (nameIndex1 === -1 || admissionNoIndex1 === -1 || yearNameIndex1 === -1 ||
             nameIndex2 === -1 || schemeIndex2 === -1 || applicationNoIndex2 === -1 ||
             courseIndex2 === -1 || disbursedAmountIndex2 === -1) {
-            console.error("Invalid column indices. Please check header names.")
-            return
+            console.error("Invalid column indices. Please check header names.");
+            return;
         }
 
-        const flattenedData2 = rows2.map(row => ({
-            name: standardizeName(row[nameIndex2]?.toString().trim()),
-            disbursedAmount: parseFloat(row[disbursedAmountIndex2]) || 0,
-            scheme: row[schemeIndex2],
-            applicationNo: row[applicationNoIndex2],
-            course: row[courseIndex2],
-        }))
+        const aggregatedData2 = rows2.reduce((acc, row) => {
+            const name = standardizeName(row[nameIndex2]?.toString().trim());
+            if (!acc[name]) {
+                acc[name] = {
+                    disbursedAmount: 0,
+                    scheme: row[schemeIndex2],
+                    applicationNo: row[applicationNoIndex2],
+                    course: row[courseIndex2]
+                };
+            }
+            acc[name].disbursedAmount += parseFloat(row[disbursedAmountIndex2]) || 0;
+            return acc;
+        }, {});
 
+        console.log(aggregatedData2);
 
-        console.log(flattenedData2)
-        const fuse = new Fuse(flattenedData2, { keys: ["name"], includeScore: true, threshold: 0.25 })
+        const fuse = new Fuse(Object.keys(aggregatedData2).map(name => ({ name })), {
+            keys: ["name"],
+            includeScore: true,
+            threshold: 0.25
+        });
 
         const merged = rows1.map(row1 => {
-            const name1 = standardizeName(row1[nameIndex1]?.toString().trim())
-            const name=row1[nameIndex1]?.toString().trim()
-            console.log(name1)
+            const name1 = standardizeName(row1[nameIndex1]?.toString().trim());
+            const name = row1[nameIndex1]?.toString().trim();
             const yearName = row1[yearNameIndex1];
-            console.log(fuse.search(name1)[0]?.item || {})
             const bestMatch = fuse.search(name1)[0]?.item || {};
+            const aggregatedData = aggregatedData2[bestMatch.name] || {};
 
             const totalFee = feeMapping[yearName] || 0;
-            const disbursedAmount = bestMatch.disbursedAmount || 0;
+            const disbursedAmount = aggregatedData.disbursedAmount || 0;
             const remainingFee = totalFee - disbursedAmount;
-            if (bestMatch == {}) return null
+            if (Object.keys(aggregatedData).length === 0) return null;
+
             return [
                 name,
                 row1[admissionNoIndex1],
                 yearName,
-                bestMatch.scheme || '',
-                bestMatch.applicationNo || '',
-                bestMatch.course || '',
+                aggregatedData.scheme || '',
+                aggregatedData.applicationNo || '',
+                aggregatedData.course || '',
                 disbursedAmount,
                 remainingFee
             ];
-        });
-
+        }).filter(row => row !== null); // Remove null rows
 
         const finalHeader = [
             'Name', 'Admission_No', 'YearName',
             'Scheme', 'Application_No', 'Course', 'Disbursed_Amount', 'Remaining_Fee'
-        ]
-        setMergedData([finalHeader, ...merged])
-
+        ];
+        setMergedData([finalHeader, ...merged]);
     }
-
 
     const handleDownload = () => {
-        const worksheet = XLSX.utils.aoa_to_sheet(mergedData)
-        const workbook = XLSX.utils.book_new()
-        XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1")
+        const worksheet = XLSX.utils.aoa_to_sheet(mergedData);
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
 
-        const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' })
-        const excelBlob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' })
+        const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+        const excelBlob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
 
-        const downloadLink = URL.createObjectURL(excelBlob)
-        const link = document.createElement('a')
-        link.href = downloadLink
-        link.download = "merged_data.xlsx"
-        link.click()
+        const downloadLink = URL.createObjectURL(excelBlob);
+        const link = document.createElement('a');
+        link.href = downloadLink;
+        link.download = "merged_data.xlsx";
+        link.click();
 
         setTimeout(() => {
-            URL.revokeObjectURL(downloadLink)
-        }, 100)
+            URL.revokeObjectURL(downloadLink);
+        }, 100);
     }
-
 
     return (
         <Box padding={'10px'}>
@@ -216,8 +233,15 @@ const Home = () => {
                         <FormErrorMessage>{formik.errors.file2}</FormErrorMessage>
                     </FormControl>
 
-                    <Box display='flex' justifyContent={'center'} alignItems={'center'} width='100%' py='10px' my='20px'>
-                        <Button width='90%' height='45px' borderRadius='20px' colorScheme='linkedin' type='submit'>Submit</Button>
+                    <Box display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                        <Button
+                            mt={4}
+                            colorScheme='teal'
+                            isLoading={formik.isSubmitting}
+                            type='submit'
+                        >
+                            Submit
+                        </Button>
                     </Box>
                 </form>
             </Box>
@@ -232,7 +256,6 @@ const Home = () => {
                 />
             </Box>
             {mergedData.length > 0 && (
-
                 <Box mt='20px'>
                     <Box display='flex' justifyContent={'center'} alignItems={'center'} width='100%' py='10px' my='20px'>
                         <Button width='20%' height='45px' borderRadius='20px' colorScheme='blue' onClick={handleDownload}>
@@ -262,12 +285,10 @@ const Home = () => {
                             ))}
                         </Tbody>
                     </Table>
-
-
                 </Box>
             )}
         </Box>
-    )
+    );
 }
 
-export default Home 
+export default Home;
