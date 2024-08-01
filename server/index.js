e3fd45b7646d56eb7dda79bae8cd981c6047ba24
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2024-08-01 22:56:29 +0530

    folder structure changed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express, { urlencoded } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import path from 'path'
 import connectToDB from './db/database.js'
 import routerAuth from './routes/authRoutes.js'
 import routerFee from './routes/feeRoutes.js'
 dotenv.config()
 const app = express()
+const __dirname=path.resolve()
 
 app.use(express.json())
 
@@ -14,7 +16,11 @@ app.use(cors())
 app.use('/api/auth',routerAuth)
 app.use('/api/fee',routerFee)
 
+app.use(express.static(path.join(__dirname,"/client/build")))
 
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"client","build","index.html"))
+})
 
 
 connectToDB()
